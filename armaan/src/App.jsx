b200df5e9718d53b1352cf36a92ebc2022653b2a
Armaansaini20 <113324811+Armaansaini20@users.noreply.github.com>
2024-11-07 13:30:25 +0530

    app.jsx change

--- armaan/src/App.jsx
+++ armaan/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import Header from './components/header';
+import Header from './components/Header';
 import Home from './components/Home';
-import Footer from './components/footer';
-import Benefits from './components/benefits';
+import Footer from './components/Footer';
+import Benefits from './components/Benefits';
 
 const App = () => {
   return (
