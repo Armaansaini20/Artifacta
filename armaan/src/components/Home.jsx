5e59484dcf4ad497589fdc9a34ed94e2d9229a74
Armaansaini20 <113324811+Armaansaini20@users.noreply.github.com>
2024-11-11 09:22:53 +0530

    show

--- armaan/src/components/Home.jsx
+++ armaan/src/components/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
       <div id='home' className='w-full bg-black '>
       <WavyBackground className="max-w-4xl mx-auto py-40">
       <p className="text-4xl md:text-6xl lg:text-8xl font-sora font-bold tracking-tighter text-slate-50">
-            Explore the Possibilities of AI with Artifacta
+            Explore the RAMIT of AI with Artifacta
       </p>
       <p className="text-lg md:text-2xl lg:text-3xl font-sora font-bold text-slate-100 mt-4 md:mt-8">
             Unleash the Power of AI with Artifacta. Artifacta leverages AI to help you from gathering the missing words of a manuscript to getting a totally restored 3D model of your provided image.
